5a943d3e0318e627a5fd340a2b4bc947d94df88f
gcasta11 <gcasta@uw.edu>
2024-12-29 01:11:16 -0800

    made changes to core 3 files for debugging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from '../assets/img/logo.png'; // If App.js is in `src/components/`
+import React from 'react';
+import { CustomNavBar } from './components/NavBar'; // Import updated name
 import './App.css';
-import NavBar from './components/NavBar';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div>
+      <CustomNavBar />
     </div>
   );
 }
